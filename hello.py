85c9457e8818bacf9e816abd2512236e90b35272
anukultest <118854337+anukultest@users.noreply.github.com>
2023-01-17 21:01:00 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -23,3 +23,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 st.dataframe(fruityvice_normalized)
 
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
