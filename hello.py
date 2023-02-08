3cac9907dde469a9953daff9c9e0f9a876fd98f7
anukultest <118854337+anukultest@users.noreply.github.com>
2023-02-07 21:37:23 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -26,5 +26,11 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 st.dataframe(fruityvice_normalized)
 
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_row = my_cur.fetchone()
 
+st.text(my_data_row)
 
